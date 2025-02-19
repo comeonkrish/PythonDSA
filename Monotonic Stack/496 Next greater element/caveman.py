ecfbc0949db9e7c0a244f382cde9ada093644e80
Krish.Kathuria <krish.kathuria@geminisolutions.com>
2025-02-19 11:36:05 +0530

    write caveman.py for 496

---
+++ Monotonic Stack/496 Next greater element/caveman.py
@@ -0,0 +1,32 @@
+class Solution:
+    def nextGreaterElement(self, nums1, nums2):
+
+        nums1.reverse()
+
+        ans = []
+
+        while nums1:
+            a = nums1.pop()
+            # print(a)
+            flag = False
+            for i in range(len(nums2)):
+                if a == nums2[i]:
+                    # print(f"found: {a}")
+                    flag = True
+
+                if flag and nums2[i] > a:
+                    # print(f"found greater element {nums2[i]}")
+                    ans.append(nums2[i])
+                    break
+
+                if flag and i == (len(nums2) - 1):
+                    ans.append(-1)
+            
+            # print(ans)
+
+        return ans
+
+obj = Solution()
+answer = obj.nextGreaterElement([], [])
+print(answer)
+        
