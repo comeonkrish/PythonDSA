67b00f848e06ecd4ce2774da57d298cf4bf2d030
Krish.Kathuria <krish.kathuria@geminisolutions.com>
2025-02-21 11:49:23 +0530

    write solution for 347

---
+++ Top K Elements/347 Top K Frequent Elements/347.py
@@ -0,0 +1,33 @@
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums, k):
+       
+        freq_map = {}
+    
+        for num in nums:
+            if num in freq_map:
+                freq_map[num] += 1
+            else:
+                freq_map[num] = 1
+    
+        heap = []
+    
+        for num, freq in freq_map.items():
+            heapq.heappush(heap, (freq, num))  
+        
+        
+        if len(heap) > k:
+            heapq.heappop(heap)
+    
+        top_k_elements = []
+    
+        while heap:
+            _, num = heapq.heappop(heap) 
+            top_k_elements.append(num)  
+    
+        return top_k_elements
+    
+obj = Solution()
+ans = obj.topKFrequent([1,1,1,2,2,3], 2)
+print(ans)
